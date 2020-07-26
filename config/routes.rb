69df325d6c03c83f9e125e7790ef500e36124fbd
Sounn <kikuitisounn@gmail.com>
2020-07-26 17:33:49 +0900

    add/Body/create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root to: "bodies#index"
   devise_for :users
+  scope :bodies do
+    get 'index', to:'bodies#index', as:'bodies_index'
+    get 'new', to:'bodies#new',as:'bodies_new'
+    post 'create', to:'bodies#create',as:'bodies_create'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #メール確認用
   if Rails.env.development?
