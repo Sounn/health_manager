6440595b7174f1b10db735823a3f45c6c05bc2be
Sounn <kikuitisounn@gmail.com>
2020-08-04 17:08:44 +0900

    fix/body/create,index,seed.rb..

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -1,10 +1,14 @@
 class BodiesController < ApplicationController
   before_action :authenticate_user!
+  before_action :block_second_body, only: [ :new]
+
   def index_week
-    @bodies = Body.where(user_id:1).where('created_at >= ?',1.week.ago)   
+    @bodies = Body.where(user_id:current_user.id).where('created_at >= ?',1.week.ago).order(created_at: "DESC")
+    set_create_or_edit_path
   end
   def index_month
-    @bodies = Body.where(user_id:1).where('created_at >= ?',1.month.ago)
+    @bodies = Body.where(user_id:current_user.id).where('created_at >= ?',1.month.ago).order(created_at: "DESC")
+    set_create_or_edit_path
   end
 
   def new
@@ -46,4 +50,29 @@ class BodiesController < ApplicationController
     def body_params
       params.require(:body).permit(:height,:weight,:fat_percentage)
     end
+
+    def set_create_or_edit_path
+      if @bodies.first == nil #降順にしているからfirst
+        flash[:warning] = "計測結果を入力してください。"
+        redirect_to bodies_new_path
+        return #作成
+      end
+      if @bodies.first.created_at.strftime('%F') == Time.zone.now.strftime('%F')
+        @path = bodies_edit_path(@bodies.first.id) #編集
+        @flag = "編集"
+      else
+        @path = bodies_new_path #作成
+        @flag = "作成"
+      end
+    end
+
+    def block_second_body
+      body = Body.where(user_id: current_user.id).last
+      if body.created_at.strftime('%F') == Time.zone.now.strftime('%F')
+        flash[:warning] = "既に今日の計測結果は入力されています。"
+        redirect_to root_path#作成
+      end
+    end
+
+
 end
